5412b5b3157519323f36f649faa0752098e9b59d
Vishal Gupta <vishal.1991.gupta@gmail.com>
2026-02-27 14:02:44 +0530

    test

--- snapshot-cleanup/modules/iam/main.tf
+++ snapshot-cleanup/modules/iam/main.tf
@@ -15,7 +15,7 @@ resource "aws_iam_role" "lambda_role" {
 
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
   role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
 resource "aws_iam_role_policy" "ec2_snapshot_policy" {
