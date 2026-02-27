a116c6b5ec6e877f0b1a2b4d8cea7d1299bda243
Vishal Gupta <vishal.1991.gupta@gmail.com>
2026-02-27 12:57:41 +0530

    adding files

---
+++ snapshot-cleanup/modules/iam/main.tf
@@ -0,0 +1,46 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_snapshot_cleanup_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy" "ec2_snapshot_policy" {
+  name = "ec2_snapshot_policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeSnapshots",
+          "ec2:DeleteSnapshot"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+output "lambda_role_arn" {
+  value = aws_iam_role.lambda_role.arn
+}
+
+output "lambda_role_name" {
+  value = aws_iam_role.lambda_role.name
+}
---
+++ snapshot-cleanup/modules/vpc/main.tf
@@ -0,0 +1,28 @@
+resource "aws_vpc" "lambda_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.lambda_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.aws_region}a"
+}
+
+resource "aws_security_group" "lambda_sg" {
+  name   = "lambda-sg"
+  vpc_id = aws_vpc.lambda_vpc.id
+}
+
+# Outputs
+output "vpc_id" {
+  value = aws_vpc.lambda_vpc.id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private_subnet[*].id
+}
+
+
+output "lambda_security_group_id" {
+  value = aws_security_group.lambda_sg.id
+}
